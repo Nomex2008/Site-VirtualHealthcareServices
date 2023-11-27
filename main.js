418c152f6bb707513b60f097c3e10784ff604ec2
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-27 23:18:20 +0200

    Commit 10 (burger)
    Finish burger add nav bar.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 "use strict"
 
 const burger = document.getElementById('burger')
+const nav = document.getElementById('nav')
+const main = document.getElementById('main')
 const bodyClassList = document.body.classList
 //console.log(burger)
+//console.log(nav)
+//console.log(main)
 //console.log(bodyClassList)
 
 burger.addEventListener('click',burgerClick)
@@ -10,4 +14,6 @@ burger.addEventListener('click',burgerClick)
 function burgerClick() {
     bodyClassList.toggle('hidden')
     burger.classList.toggle('active')
+    nav.classList.toggle('active')
+    main.classList.toggle('active')
 }
