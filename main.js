661095083d3aec48b8dd71884d42b70e349b5dbd
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-11-27 22:34:12 +0200

    Commit 9 (burger 2)
    Add transform affect for burger after click.

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-'use strict'
+"use strict"
+
+const burger = document.getElementById('burger')
+const bodyClassList = document.body.classList
+//console.log(burger)
+//console.log(bodyClassList)
+
+burger.addEventListener('click',burgerClick)
+
+function burgerClick() {
+    bodyClassList.toggle('hidden')
+    burger.classList.toggle('active')
+}
